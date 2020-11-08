4dbe6c255e12444e5c727413d16abb90c47f6dc8
Nikhil Kumar Yadav <nikhilyadavindigo@gmail.com>
2020-11-08 22:15:15 +0530

    Cheapest_hotel_Feature_Added

--- HotelManagement/HotelReservation.cs
+++ HotelManagement/HotelReservation.cs
@@ -20,8 +20,40 @@ namespace HotelReservationSystem
                 Console.WriteLine("Hotel Already Exists");
                 return;
             }
-            Console.WriteLine("Hotel " + hotel.name + " Added");
             hotels.Add(hotel.name, hotel);
+        }
+
+        public Hotel FindCheapestHotel(DateTime startDate, DateTime endDate)
+        {
+            if (startDate > endDate)
+            {
+                Console.WriteLine("Start date cannot be greater than end date");
+            }
+
+            var cost = Int32.MaxValue;
+            Hotel cheapestHotel = new Hotel();
+            foreach (var hotel in hotels)
+            {
+                var temp = cost;
+                cost = Math.Min(cost, CalculateTotalCost(hotel.Value, startDate, endDate));
+                if (temp != cost)
+                    cheapestHotel = hotel.Value;
+            }
+            return cheapestHotel;
+        }
+        public int CalculateTotalCost(Hotel hotel, DateTime startDate, DateTime endDate)
+        {
+            var cost = 0;
+            var weekdayRate = hotel.weekdayRatesRegular;
+            var weekendRate = hotel.weekendRatesRegular;
+            for (DateTime date = startDate; date <= endDate; date = date.AddDays(1))
+            {
+                if (date.DayOfWeek == DayOfWeek.Sunday || date.DayOfWeek == DayOfWeek.Saturday)
+                    cost += weekendRate;
+                else
+                    cost += weekdayRate;
+            }
+            return cost;
         }
     }
 }
--- HotelManagement/Program.cs
+++ HotelManagement/Program.cs
@@ -9,18 +9,45 @@ namespace HotelReservationSystem
             Console.WriteLine("Welcome to Hotel Reservation System!");
 
             var hotelReservation = new HotelReservation();
-            var hotel = new Hotel();
-            Console.Write("Enter Hotel Name : ");
-            hotel.name = Console.ReadLine();
+            bool val = true;
+            //Console.WriteLine("How many hotels do you want?");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            while (val)
+            {
+                var hotel = new Hotel();
+                Console.Write("Enter Hotel Name : ");
+                hotel.name = Console.ReadLine();
 
-            Console.Write("Enter Regular Weekday Rate : ");
-            hotel.weekdayRatesRegular = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter Regular Weekday Rate : ");
+                hotel.weekdayRatesRegular = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Enter Regular Weekend Rate : ");
-            hotel.weekendRatesRegular = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter Regular Weekend Rate : ");
+                hotel.weekendRatesRegular = Convert.ToInt32(Console.ReadLine());
 
-            hotelReservation.AddHotel(hotel);
-            //hotelReservation.InitializeConsoleIO();
+                hotelReservation.AddHotel(hotel);
+
+                Console.WriteLine("Wanna add more hotels?(yes/no)");
+                if (Console.ReadLine() == "no")
+                    val = false;
+            }
+
+            //UC2
+            Console.Write("Enter the date range : ");
+            var input = Console.ReadLine();
+            string[] dates = input.Split(',');
+            try
+            {
+                var startDate = Convert.ToDateTime(dates[0]);
+                var endDate = Convert.ToDateTime(dates[1]);
+                var cheapestHotel = hotelReservation.FindCheapestHotel(startDate, endDate);
+                var cost = hotelReservation.CalculateTotalCost(cheapestHotel, startDate, endDate);
+                Console.WriteLine("Hotel : {0}, Total Cost : {1}", cheapestHotel.name, cost);
+            }
+            catch
+            {
+                Console.Write("Enter the correct date range : ");
+            }
+
         }
     }
 }
